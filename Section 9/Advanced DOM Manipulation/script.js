9631a896266db31f1d2a0fadef1d16bb3520a070
Bryce Perez <bryceperez31@gmail.com>
2021-12-01 16:24:11 -0700

    section reveal animation

--- Section 9/Advanced DOM Manipulation/script.js
+++ Section 9/Advanced DOM Manipulation/script.js
@@ -147,6 +147,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal sections ////////////////////////////////////////////////
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 /////////////////Smooth Scroll///////////////////////////////////////////////
 
 btnScrollTo.addEventListener("click", function (e) {
@@ -184,7 +206,7 @@ btnScrollTo.addEventListener("click", function (e) {
 // console.log(document.body);
 
 // const header = document.querySelector(".header");
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
 // console.log(allSections);
 
