56963c218f35aa260077df66b33d46f4eb70b494
Bryce Perez <bryceperez31@gmail.com>
2021-12-01 11:15:05 -0700

    complete tabbed component

--- Section 9/Advanced DOM Manipulation/script.js
+++ Section 9/Advanced DOM Manipulation/script.js
@@ -58,6 +58,32 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+// Tabbed components ////////////////////////////////////////////////////////
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  // console.log(clicked);
+
+  // guard clause
+  if (!clicked) return;
+
+  // Active Tab
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  // Active content area
+  // console.log(clicked.dataset.tab);
+  tabsContent.forEach((tab) =>
+    tab.classList.remove("operations__content--active")
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
 /////////////////Smooth Scroll///////////////////////////////////////////////
 
 btnScrollTo.addEventListener("click", function (e) {
