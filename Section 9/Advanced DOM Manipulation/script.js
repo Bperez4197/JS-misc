d3ecd77e5a7b2e03a0c2df5ef16951468481bf24
Bryce Perez <bryceperez31@gmail.com>
2021-11-30 20:00:12 -0700

    Useful dom manipulation reminders

---
+++ Section 9/Advanced DOM Manipulation/script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((button) => button.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+
+// console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName("btn"));
+
+// Creating and inserting elements///////////////////////////////////////
+// .insertAdjacentHTML
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.textContent =
+  "We use cookies for improved functionality and analytics.";
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got It!</button>';
+
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true)); // If I want a dom node to be in two places at once I must clone it like this
+
+header.before(message);
+// header.after(message);
+
+// Delete elements///////////////////////////////////////////////
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
+
+// Styles ///////////////////////////////////////
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+// console.log(message.style.height); //only works for inline styles such as the ones we set ourselves
+//console.log(message.style.backgroundColor);// this works because we set it here in the js which makes it inline styling
+
+// console.log(getComputedStyle(message).color); // use this to get non-inline styling
+
+// These two methods to change values of properties that aren't inline styles
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+// document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes ///////////////////////////////////////////
+const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.className);
+
+logo.alt = "Beautiful minimalist logo";
+
+// Non-standard
+// console.log(logo.designer); //doesnt work
+//console.log(logo.getAttribute('designer')); // works
+logo.setAttribute("company", "Bankist");
+
+// console.log(logo.src);
+// console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
+
+// // Data Attributes //////////////////////////////////////////////////
+// // start with data
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add("c", "j");
+// logo.classList.remove("c");
+// logo.classList.toggle("c");
+// logo.classList.contains("c", "j");
+
+// //Don't USE
+// logo.className = 'bryce';
