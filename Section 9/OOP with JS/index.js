6adbd948efd96caa30a23b31da582d6c3007a20e
Bryce Perez <bryceperez31@gmail.com>
2021-12-02 14:33:35 -0700

    getters and setters

--- Section 9/OOP with JS/index.js
+++ Section 9/OOP with JS/index.js
@@ -60,8 +60,8 @@ Person.prototype.species = "Homo Sapiens";
 ////////////////////////// ES6 CLASSES /////////////////////////////////////////////////// //
 
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -70,15 +70,57 @@ class PersonCl {
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exits////////////////////////
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl("Jessica", 1996);
-console.log(jessica);
-jessica.calcAge();
-console.log(jessica.__proto__ === PersonCl.prototype);
+const jessica = new PersonCl("Jessica Davis", 1996);
+// console.log(jessica);
+// jessica.calcAge();
+// console.log(jessica.__proto__ === PersonCl.prototype);
+// console.log(jessica.age);
 
+//This is the same as declaring the method outside the constructor
 PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}!`);
+  console.log(`Hey ${this.fullName}!`);
 };
 
 jessica.greet();
+
+//1. Classes are NOT hoisted which means we cant use them before they're declared
+//2. classes are first-class citizens which means we can pass them to functions and return them from functions
+//3. Classes are executed in strict mode
+
+////////////////////////////////////GETTERS AND SETTERS/////////////////////////////////////////////////////////
+const walter = new PersonCl("Walter White", 1965);
+
+const account = {
+  owner: "bryce",
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+
+console.log(account.movements);
