1da7067a693837da9592ebf8c11a69ba7806b932
Bryce Perez <bryceperez31@gmail.com>
2021-12-06 15:06:51 -0700

    asynchronous javascript before ES6's Promises

--- Section 9/Asynchronous Javascript/script.js
+++ Section 9/Asynchronous Javascript/script.js
@@ -1,6 +1,98 @@
 "use strict";
+// https://restcountries.com/v2/
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// old way of doing it XML style ///////////////////////////////////////////////////////////////////////
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     // console.log(data);
+
+//     const html = `<article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               Number(data.population) / 1000000
+//             ).toFixed(1)}M people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].name
+//             }</p>
+//           </div>
+//         </article>`;
+
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData("portugal");
+// getCountryData("usa");
+// getCountryData("germany");
+
+/////////////////////////////// Controlling the order of execution ///////////////////////////////////
+/// using a callback inside of a callback to control the order of execution //////////////////////////////////
+/// This leads to callback hell ///////////////////////////////////
+
+const renderCountry = function (data, className = "") {
+  const html = `<article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              Number(data.population) / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  // Ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+// getCountryAndNeighbor("portugal");
+getCountryAndNeighbor("usa");
