7bba7e638059ae69a39787bea246c9da4d848977
Bryce Perez <bryceperez31@gmail.com>
2021-12-07 11:56:09 -0700

    Other promise combinators

--- Section 9/Asynchronous Javascript/script.js
+++ Section 9/Asynchronous Javascript/script.js
@@ -404,3 +404,46 @@ const get3Countries = async function (c1, c2, c3) {
   }
 };
 get3Countries("portugal", "canada", "tanzania");
+
+////////// Other promise combinators ////////////////////////////////////////////////
+// Promise.race returns one result. the result of the promise that finished first
+(async function () {
+  const response = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+  ]);
+
+  //   console.log(response[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error("Request took too long"));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/tanzania`),
+  timeout(1),
+])
+  .then((res) => console.log(res[0]))
+  .catch((err) => console.error(err));
+
+/// Promise.allSettled //////////////////////////////////////
+
+Promise.allSettled([
+  Promise.resolve("success"),
+  Promise.reject("error"),
+  Promise.resolve("success"),
+]).then((res) => console.log(res));
+
+// Promise.any //////
+
+Promise.any([
+  Promise.resolve("success"),
+  Promise.reject("error"),
+  Promise.resolve("success"),
+]).then((res) => console.log(res));
