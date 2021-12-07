cc1067998f524e6cc5140dc775a3ff8a891016e7
Bryce Perez <bryceperez31@gmail.com>
2021-12-06 18:20:02 -0700

    handling promises practice and code challenge

--- Section 9/Asynchronous Javascript/script.js
+++ Section 9/Asynchronous Javascript/script.js
@@ -62,12 +62,12 @@ const renderCountry = function (data, className = "") {
         </article>`;
 
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 const getCountryAndNeighbor = function (country) {
@@ -186,5 +186,37 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener("click", function () {
-  getCountryData("usa");
+  //   getCountryData("usa");
+  whereAmI(19.037, 72.873);
 });
+
+function whereAmI(lat, lng) {
+  return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(
+          `You made more than 3 requests in 5 seconds. Please wait to send another request`
+        );
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(`You are in ${data.city},${data.country}.`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Country not found");
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((error) => console.error(error.message));
+}
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
